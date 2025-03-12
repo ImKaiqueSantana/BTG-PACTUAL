7d648fede8eaa4058aaf914461b7441c294e37fd
Nathan Barros <nathansousa0809@gmail.com>
2025-03-12 08:32:23 -0300

    total value and summary add

--- src/main/java/com/gruposv/btgpactual/orderms/service/OrderService.java
+++ src/main/java/com/gruposv/btgpactual/orderms/service/OrderService.java
@@ -5,19 +5,32 @@ import com.gruposv.btgpactual.orderms.dtos.OrderResponse;
 import com.gruposv.btgpactual.orderms.entity.OrderEntity;
 import com.gruposv.btgpactual.orderms.entity.OrderItem;
 import com.gruposv.btgpactual.orderms.repostory.OrderRepostory;
+import org.bson.Document;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
 import java.util.List;
+
+import static org.springframework.data.mongodb.core.aggregation.Aggregation.*;
 
 @Service
 public class OrderService {
 
 
     private final OrderRepostory orderRepostory;
-    public OrderService(OrderRepostory orderRepostory) {this.orderRepostory = orderRepostory;}
+    private final MongoTemplate mongoTemplate;
+
+
+
+
+
+    public OrderService(OrderRepostory orderRepostory, MongoTemplate mongoTemplate) {this.orderRepostory = orderRepostory;
+        this.mongoTemplate = mongoTemplate;
+    }
 
 
     public void save(OrderCreatedEvent event){
@@ -35,6 +48,14 @@ public class OrderService {
         return orders.map(OrderResponse::fromEntity);
     }
 
+    public BigDecimal findTotalOnOdersByCustomerId(Long customerId){
+    var aggregations  = newAggregation(
+            match(Criteria.where("customerId").is(customerId)),
+            group().sum("total").as("total")
+    );
+        var response = mongoTemplate.aggregate(aggregations,"tb_orders", Document.class);
+        return new BigDecimal(response.getUniqueMappedResult().get("total").toString());
+    }
     private BigDecimal getTotal(OrderCreatedEvent event) {
         return event.itens()
                 .stream()
